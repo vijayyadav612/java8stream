7e9b7fd0e4292a7fb1b5e6abc26441a8c02a11e0
VJ Yadav <jsevijay@gmail.com>
2023-02-11 20:02:28 +0530

    inital commit

---
+++ src/stream/CharFrequency.java
@@ -0,0 +1,16 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "vijay kumar yadav";
+        Map<String, Long> countMap = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+       System.out.println(countMap);
+
+    }
+}

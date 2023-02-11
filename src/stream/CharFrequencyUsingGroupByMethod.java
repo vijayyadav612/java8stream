85130f13cba723e18d6b9fc2855992e7a558890f
VJ Yadav <jsevijay@gmail.com>
2023-02-11 20:25:03 +0530

    inital commit

--- src/stream/CharFrequencyUsingGroupByMethod.java
+++ src/stream/CharFrequencyUsingGroupByMethod.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class CharFrequency {
+public class CharFrequencyUsingGroupByMethod {
     public static void main(String[] args) {
         String str = "vijay kumar yadav";
         Map<String, Long> countMap = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
